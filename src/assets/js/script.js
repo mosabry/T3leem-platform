0dab0662cf694c4c3f7252c79851ad4dd2b07a4b
Mohamed Sabry <44135662+IMSabry@users.noreply.github.com>
2020-08-17 00:07:20 +0200

    update

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -14,3 +14,16 @@ window.addEventListener("load", function () {
         },
     });
 });
+
+/* Zoom on Scroll */
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    $(".zoom").css({
+        backgroundSize: (100 + scroll / 5) + "%",
+        top: -(scroll / 10) + "%",
+
+        //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
+        //"-webkit-filter": "blur(" + (scroll/200) + "px)",
+        //filter: "blur(" + (scroll/200) + "px)"
+    });
+});
