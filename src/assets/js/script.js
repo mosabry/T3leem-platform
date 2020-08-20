732e9441ac00d1915349fd9e4db9f8715bda07d2
Mohamed Sabry <44135662+IMSabry@users.noreply.github.com>
2020-08-20 02:42:04 +0200

    Add Course Detail Page

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,11 +3,11 @@ new WOW().init();
 
 /* Initialize Gliderjs */
 window.addEventListener("load", function () {
-    new Glider(document.querySelector('.glider'), {
+    new Glider(document.querySelector(".glider"), {
         slidesToScroll: 1,
         slidesToShow: 4.5,
         draggable: true,
-        dots: '.dots',
+        dots: ".dots",
         duration: 0.25,
         responsive: [
             {
@@ -15,33 +15,33 @@ window.addEventListener("load", function () {
                 breakpoint: 120,
                 settings: {
                     // Set to `auto` and provide item width to adjust to viewport
-                    slidesToShow: '1',
-                    slidesToScroll: '1',
+                    slidesToShow: "1",
+                    slidesToScroll: "1",
                     itemWidth: 50,
-                    duration: 0.25
-                }
+                    duration: 0.25,
+                },
             },
             {
                 // screens greater than >= 440px
                 breakpoint: 440,
                 settings: {
                     // Set to `auto` and provide item width to adjust to viewport
-                    slidesToShow: '1.5',
-                    slidesToScroll: '1',
+                    slidesToShow: "1.5",
+                    slidesToScroll: "1",
                     itemWidth: 50,
-                    duration: 0.25
-                }
+                    duration: 0.25,
+                },
             },
             {
                 // screens greater than >= 775px
                 breakpoint: 775,
                 settings: {
                     // Set to `auto` and provide item width to adjust to viewport
-                    slidesToShow: '2.5',
-                    slidesToScroll: '1',
+                    slidesToShow: "2.5",
+                    slidesToScroll: "1",
                     itemWidth: 150,
-                    duration: 0.25
-                }
+                    duration: 0.25,
+                },
             },
             {
                 // screens greater than >= 1024px
@@ -50,9 +50,9 @@ window.addEventListener("load", function () {
                     slidesToShow: 3.5,
                     slidesToScroll: 1,
                     itemWidth: 150,
-                    duration: 0.25
-                }
-            }
+                    duration: 0.25,
+                },
+            },
         ],
         // arrows: {
         //     prev: '.glider-prev',
@@ -74,6 +74,34 @@ $(window).scroll(function () {
     });
 });
 
+/* Config Zoom on small device */
 if ($(window).width() < 768) {
-    $('header').removeClass('zoom')
+    $("header").removeClass("zoom");
 }
+
+/* Open Modal */
+$(document).ready(function () {
+    // Gets the video src from the data-src on each button
+    var $videoSrc;
+    $(".video-btn").click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+
+    // when the modal is opened autoplay it
+    $("#myModal").on("shown.bs.modal", function (e) {
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr(
+            "src",
+            $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+        );
+    });
+
+    // stop playing the youtube video when I close the modal
+    $("#myModal").on("hide.bs.modal", function (e) {
+        // a poor man's stop video
+        $("#video").attr("src", $videoSrc);
+    });
+
+    // document ready
+});
